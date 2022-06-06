a8d90cac5c7b00ca92ac5eb42c4de9a382c9ee88
wisdom <wisdom08@kakao.com>
2022-06-06 12:46:40 +0900

    DiaryEditor 컴포넌트 개선

--- src/component/DiaryEditor.js
+++ src/component/DiaryEditor.js
@@ -1,35 +1,67 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 import '../style/DiaryEditor.css';
 
 const DiaryEditor = () => {
-    let [author, setAuthor] = useState('변지혜');
-    let [content, setContent] = useState('');
-    let [score, setScore] = useState(5);
 
-    console.log(author, content, score);
+
+    let [state, setState] = useState({
+        author: '',
+        content: '',
+        emotion: 5
+    });
+
+    const authorInput = useRef();
+    const contentInput = useRef();
+
+    const handleChangeState = (e) => {
+        setState({
+            ...state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = () => {
+        if (state.author.length < 1) {
+            authorInput.current.focus();
+            return;
+        }
+
+        if (state.content.length < 5) {
+            contentInput.current.focus();
+            return;
+        }
+
+        alert('저장 성공입니다.');
+    }
 
     return (
         <div className="DiaryEditor">
             <h2>오늘의 일기</h2>
             <div>
                 <div>
-                    <input placeholder="writer" type="text" value={author}
-                           onChange={(e) => setAuthor(e.target.value)}/>
+                    <input name="author"
+                           placeholder="writer" type="text" value={state.author}
+                           ref = {authorInput}
+                           onChange={handleChangeState}/>
                 </div>
                 <div>
-                    <textarea name="" id="" cols="30" rows="10" placeholder="contents" value={content}
-                              onChange={(e) => setContent(e.target.value)}
-                    ></textarea>
+                    <textarea name="content" id="" cols="30" rows="10" placeholder="contents" value={state.content}
+                              ref={contentInput}
+                              onChange={handleChangeState}></textarea>
                 </div>
                 <div>
-                    <label htmlFor="">오늘의 감정 점수: </label>
-                    <input type="number"
-                           placeholder="score"
-                           value={score}
-                           onChange={(e) => setScore(e.target.value)}
-                    />
+                    <span>오늘의 감정 정수: </span>
+                    <select name="emotion" value={state.emotion} onChange={handleChangeState}>
+                        <option value={1}>1</option>
+                        <option value={2}>2</option>
+                        <option value={3}>3</option>
+                        <option value={4}>4</option>
+                        <option value={5}>5</option>
+                    </select>
                 </div>
-                <button>일기 저장하기</button>
+                <div>
+                    <button onClick={handleSubmit}>일기 저장하기</button>
+                </div>
             </div>
         </div>
     )
