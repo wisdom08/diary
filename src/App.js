7861dad192595a902fc9e3e57fd617d4983129a0
wisdom <wisdom08@kakao.com>
2022-06-11 10:23:48 +0900

    일기 데이터 api 호출해서 초깃값 셋팅

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import './App.css';
 import DiaryEditor from "./component/DiaryEditor";
 import DiaryList from "./component/DiaryList";
-import {useRef, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 function App() {
     let [data, setData] = useState([]);
 
     const dataId = useRef(0);
+
+    const getData = async () => {
+        const res = await fetch(
+            "https://jsonplaceholder.typicode.com/comments"
+        ).then((res) => res.json());
+
+        const initData = res.slice(0, 20).map((it) => {
+            return {
+                author: it.email,
+                content: it.body,
+                emotion: Math.floor(Math.random() * 5) + 1,
+                created_date: new Date().getTime(),
+                id: dataId.current++
+            }
+        })
+
+        setData(initData);
+
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
 
     const onCreate = (author, content, emotion) => {
 
